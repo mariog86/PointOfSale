318f06eac92a160da6729f464d810d20b3e0b9c5
mariog86 <mariog86@gmail.com>
2015-11-27 08:07:17 +0100

    Completed integration test.

--- PointOfSale.Test/PointOfSaleTest.cs
+++ PointOfSale.Test/PointOfSaleTest.cs
@@ -1,28 +1,65 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Reflection;
-using System.Runtime.InteropServices;
 using System.Text;
 using FluentAssertions;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using NUnit.Framework.Internal;
+using NUnit.Framework;
 
 namespace PointOfSale.Test
 {
-    [TestClass]
+    [TestFixture]
     public class PointOfSaleTest
     {
         private static readonly string ProgramPath = Assembly.GetAssembly(typeof (Program)).Location;
+        private Process process;
 
-        [TestMethod]
-        public void ProductExists_When1234_ThenChf10()
+        [SetUp]
+        public void SetUp()
         {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo(ProgramPath)
+            {
+                UseShellExecute = false,
+                ErrorDialog = false,
+                RedirectStandardInput = true
+            };
+            this.process = new Process { StartInfo = processStartInfo };
+            bool processStarted = this.process.Start();
+            if (!processStarted)
+            {
+                throw new InvalidOperationException("Process did not start");
+            }
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.process.WaitForExit(1000);
+            if (!this.process.HasExited)
+            {
+                this.process.Close();
+            }
+        }
+
+        [TestCase("1234", "CHF 10.-")]
+        [TestCase("2345", "CHF 15.-")]
+        public void ProductExists_WhenBarcode_ThenCorrectPrice(string barcode, string price)
+        {
+            this.WriteToStdIn(barcode);
 
             var actual = ReadFromClient();
 
-            actual.Should().Be("CHF 10.-");
+            actual.Should().Be(price);
+        }
+
+        private void WriteToStdIn(string text)
+        {
+            using (StreamWriter inputWriter = this.process.StandardInput)
+            {
+                inputWriter.WriteLine(text);
+            }
         }
 
         private static string ReadFromClient()
@@ -31,15 +68,25 @@ namespace PointOfSale.Test
             try
             {
                 listener = new TcpListener(IPAddress.Any, 6740);
-                using (TcpClient client = listener.AcceptTcpClient())
+                listener.Start();
+                var acceptTcpClientAsync = listener.AcceptTcpClientAsync();
+                acceptTcpClientAsync.Wait(5000);
+                if (acceptTcpClientAsync.IsCompleted)
                 {
-                    using (StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8))
+                    using (TcpClient client = acceptTcpClientAsync.Result)
                     {
-                        string actual;
-                        actual = reader.ReadLine();
-                        return actual;
-                    }
+                        using (StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8))
+                        {
+                            string actual;
+                            actual = reader.ReadLine();
+                            return actual;
+                        }
 
+                    }
+                }
+                else
+                {
+                    return null;
                 }
             }
             finally
--- PointOfSale/Program.cs
+++ PointOfSale/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,28 @@ namespace PointOfSale
     {
         static void Main(string[] args)
         {
+            string barcode = Console.ReadLine();
+            string price;
+            switch (barcode)
+            {
+                case "1234":
+                    price = "CHF 10.-";
+                    break;
+                case "2345":
+                    price = "CHF 15.-";
+                    break;
+                default:
+                    throw new InvalidOperationException("Could not find barcode");
+            }
+            using (TcpClient client = new TcpClient())
+            {
+                client.Connect("localhost", 6740);
+                using (StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8))
+                {
+                    writer.WriteLine(price);
+                }
+            }
+            
         }
     }
 }
